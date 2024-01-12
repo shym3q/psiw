7c276292125138f6a67be31dbdebcc05f42f8cfd
Szymon <szgie@proton.me>
2024-01-12 21:50:11 +0100

    simple sending and receiving msg

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
+#include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
-int main {
-    
-    return 0;
+int main() {
+  int mid = msgget(0x123, 0600 | IPC_CREAT);
+  struct msgbuf
+  {
+    long type;
+    char text[1024];
+  } my_msg;
+  strcpy(my_msg.text, "Text");
+  my_msg.type =5;
+  msgsnd(mid, &my_msg, strlen(my_msg.text)+1, 0);
+  int received = msgrcv(mid, &my_msg, 1024, 5, 0);
+  printf("%i\n", received);
+  msgctl(mid, IPC_RMID, NULL);   
+  return 0;
 }

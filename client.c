d506ac477a7c7fb6fbba612b6f30f5cccf08e2e9
AgnJed <147380415+AgnJed@users.noreply.github.com>
2024-01-12 23:56:58 +0100

    client and server update

--- client.c
+++ client.c
@@ -1,7 +1,29 @@
-void send_message() {
+// What client can do:
+// -> send message with type and priority
+
+void send_to_server() {
 
 }
 
-void receive_message() {
+// -> receive message in synchronious or asynchronious way
+// synchronious: on demand
+// asynchronious: automatically
 
+void receive_from_server() {
+
+}
+
+void join(){
+    
+}
+
+void add_topic(){
+
+}
+
+int main() {
+    int choice;
+    while(1){
+        
+    }
 }
---
+++ server.c
@@ -0,0 +1,15 @@
+// User list
+struct{} clients[100];
+
+void register_new_client(){
+    
+}
+
+void send_to_clients() {
+
+}
+
+void receive_from_client() {
+
+}
+

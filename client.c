ad18e4e535bb9bd7924f33eadc07f3f2a2d8e433
shym3q <szgie@proton.me>
2024-01-13 15:40:46 +0100

    simple sending and receiving msg

--- client.c
+++ client.c
@@ -1,6 +1,14 @@
 // What client can do:
 // -> send message with type and priority
 
+#include <string.h>
+#include <sys/msg.h>
+
+typedef struct msgbuf {
+  long type;
+  char text[1024];
+} msg;
+
 void send_to_server() {
 
 }
@@ -13,17 +21,20 @@ void receive_from_server() {
 
 }
 
-void join(){
-    
+void join() {
+  int mid = msgget(0x123, 0600 | IPC_CREAT);
+  msg m;
+  strcpy(m.text, "hello world");
+  m.type = 5;
+  msgsnd(mid, &m, strlen(m.text) + 1, 0);
 }
 
 void add_topic(){
 
 }
 
-int main() {
-    int choice;
-    while(1){
-        
-    }
+int main(int argc, char *argv[])
+{
+  join();
+  return 0;
 }
--- server.c
+++ server.c
@@ -1,8 +1,15 @@
 // User list
+#include <stdio.h>
+#include <sys/msg.h>
 struct{} clients[100];
 
-void register_new_client(){
-    
+typedef struct msgbuf {
+  long type;
+  char text[1024];
+} msg;
+
+int register_new_client() {
+  return msgget(0x123, 0600 | IPC_CREAT);
 }
 
 void send_to_clients() {
@@ -13,3 +20,12 @@ void receive_from_client() {
 
 }
 
+int main(int argc, char *argv[])
+{
+  int mid = register_new_client();
+  msg m;
+  msgrcv(mid, &m, sizeof(m), 5, 0);
+  printf("%s\n", m.text);
+  msgctl(mid, IPC_RMID, NULL);
+  return 0;
+}
